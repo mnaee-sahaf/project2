82dcf23bbadd48f95f8f85dbfb89a79b246a6cc6
Muneeb Naeem <muneeb.naeem95@gmail.com>
2025-03-02 08:43:22 +0500

    updating banner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,9 @@ export function Header() {
         {/* Running Banner */}
         <div className="w-full bg-grey-600 py-2 overflow-hidden">
           <div className="whitespace-nowrap text-center text-xs sm:text-sm font-medium">
-            🚀 Next Live Cohort Starts on <span className="font-bold">{nextCohortDate}</span> - Enroll Now!
+            <a href="/liveClass" className="text-white hover:underline">
+              🚀 Next Live Cohort Starts on <span className="font-bold">{nextCohortDate}</span> - Click To Enroll Now!
+            </a>
           </div>
         </div>
         </header>
