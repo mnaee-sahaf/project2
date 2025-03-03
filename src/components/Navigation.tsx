3f8edfc1f498e7f5b5b9abb1263fca0b1af78e24
Muneeb Naeem <muneeb.naeem95@gmail.com>
2025-03-04 00:11:15 +0500

    updating name

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -60,7 +60,7 @@ export function Navigation() {
                 }
               }}
             >
-              ML Academy
+              Interactive ML
             </Link>
             <div>
               <button
